8798cc4c85cd8c6e5ba00f2b1f01b148c41f583d
maciej-koc <maciejkoc0@gmail.com>
2022-07-06 01:54:57 -0500

    Completed up to section 3 step 35, css and ExpensiveItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import './ExpensiveItem.css'
 
-export default function ExpenseItem(){
-    return(
-        <h2>Expensive Item</h2>
-    )
+export default function ExpenseItem() {
+  return (
+    <div className='expense-item'>
+      <div >April 7 2022</div>
+      <div className='expense-item__description'>
+        <h2 >Car Insurance</h2>
+        <div  className='expense-item__price'>$155</div>
+      </div>
+    </div>
+  );
 }
