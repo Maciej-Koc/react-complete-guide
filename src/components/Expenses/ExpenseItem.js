27a922822e7bf346444afd5e5860eeec5493894a
maciej-koc <maciejkoc0@gmail.com>
2022-07-07 22:54:39 -0500

    Created expense form and created new css files for forms

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,30 @@
+import React from 'react'
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
+import Card from '../UI/Card'
+
+
+
 
 function ExpenseItem(props) {
+
+  // let title = props.title;
+
+  const [title, setTitle] = React.useState(props.title);
+
+  function clickHandler(){
+    setTitle('Test')
+  }
+
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
   );
 }
 
