0e2cecb168f03dbd5a985d4b1aaf5103c6f84def
Maciej-Koc <mkoc2@asu.edu>
2022-07-10 13:00:15 -0500

    Updated ExpenseItem to remove unnessary button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,15 @@
-import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-  // let title = props.title;
-
-  const [title, setTitle] = React.useState(props.title);
-
-  function clickHandler() {
-    setTitle("Test");
-  }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
