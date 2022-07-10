6179e621fbc4491c0f1488db5b708426a34a86eb
Maciej-Koc <mkoc2@asu.edu>
2022-07-10 13:45:47 -0500

    Updated Expenses to use mapping for ExpenseItem tags

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,8 +5,6 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
 
 function Expenses(props) {
-  //Stored filtered year in a state
-
   const [year, setYear] = React.useState("2020");
 
   return (
@@ -15,26 +13,13 @@ function Expenses(props) {
         {/* Pushing State 
         https://www.youtube.com/watch?v=UrpNtB61qyo */}
         <ExpensesFilter passYear={(year) => setYear(year)} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
