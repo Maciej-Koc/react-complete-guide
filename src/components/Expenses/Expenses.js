ba086bd192eb8055e9c49f9059b7d9bf64ac4dc7
Maciej-Koc <mkoc2@asu.edu>
2022-07-10 19:32:25 -0500

    Updated Expenses.js to have correct filtering by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,13 +7,17 @@ import ExpensesFilter from "./ExpenseFilter";
 function Expenses(props) {
   const [year, setYear] = React.useState("2020");
 
+  const filteredYear = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === year;
+  });
+
   return (
     <div>
       <Card className="expenses">
         {/* Pushing State 
         https://www.youtube.com/watch?v=UrpNtB61qyo */}
         <ExpensesFilter passYear={(year) => setYear(year)} />
-        {props.items.map((expense) => (
+        {filteredYear.map((expense) => (
           <ExpenseItem
             key={expense.id} //Unique identifier required for React, add when mapping list
             title={expense.title}
